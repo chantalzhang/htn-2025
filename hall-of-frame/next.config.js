23f2985ae085b40f4ae2069670dd200c02137c68
chantalzhang <147449578+chantalzhang@users.noreply.github.com>
2025-09-13 16:25:57 -0400

    Got rid of experimental.appDir removed from next.config.js. Not allowed in Next.js 14

--- hall-of-frame/next.config.js
+++ hall-of-frame/next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
+
+
+/**experimental.appDirnot allowed anymore in Next.js 14 */
